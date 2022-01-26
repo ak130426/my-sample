400946390315b7a276b9e0ec42f20f8ee8a0dba6
ak130426 <35754845+ak130426@users.noreply.github.com>
2022-01-26 21:41:21 +0900

    ゼロパディングにする

--- 01/js/main.js
+++ 01/js/main.js
@@ -6,7 +6,15 @@ const child  = document.createElement('p');
 
 btn.addEventListener('click', () => {
   const today = new Date();
-  child.innerHTML = '今は'+(today.getMonth()+1)+'月'+today.getDate()+'日'+today.getHours()+'時'+today.getMinutes()+'分'+today.getSeconds()+'秒です。';
+  const month_ = today.getMonth()+1;
+  const month  = ('0' + month_).slice(-2);
+  const date_  = today.getDate();
+  const date   = ('0' + date_).slice(-2);
+  const hours  = today.getHours();
+  const minute = today.getMinutes();
+  const second = today.getSeconds();
+
+  child.innerHTML = '今は'+month+'月'+date+'日'+hours+'時'+minute+'分'+second+'秒です。';
   result.appendChild(child);
 
 });
